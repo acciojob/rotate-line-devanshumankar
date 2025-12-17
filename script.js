e71ce29fe1044a3d2a9780c38f4d3049637111de
Devanshu Chandu Mankar <158685271+devanshumankar@users.noreply.github.com>
2025-12-17 19:05:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const line = document.querySelector("#line");
+		let deg = 0;
+		setInterval(() => {
+			line.style.transform = `rotate(${deg}deg)`;
+			deg += 2;
+		}, 200);
